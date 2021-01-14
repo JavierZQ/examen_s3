6096d39d3d8c4c9beb1d4971653e1cd4808f9917
unknown <xavierquispezamorano@gmail.com>
2021-01-14 10:49:25 -0500

    primero

---
+++ js/scrip.js
@@ -0,0 +1,278 @@
+function mamatica(){
+var n1=10;
+var n2=5;
+suma=n1+n2;
+resta=n1-n2;
+multi=n1*n2;
+divicion=n1/n2;
+res=divicion%5;
+
+
+console.log(suma)
+console.log(resta)
+console.log(multi)
+console.log(divicion)
+console.log(residuo)
+console.log
+}
+
+function nom(){
+var String ="juan";
+console.log("bienvendo   "+String)
+
+}
+
+function impuesto(){
+var total=100;
+var subtotal;
+var IGV;
+subtotal=total/1.8;
+IGV=subtotal*0.18;
+console.log("toal "+total)
+console.log("subtotal "+subtotal)
+console.log("igv "+IGV)
+
+}
+var numero = Array(4);
+var suma;
+ 
+for (i = 0; i < numero.length; i++){
+    numero[i] = prompt("Ingrese un numero");
+ 
+    suma=numero; }
+function pre2(){
+var nu1=20;
+var nu2=10;
+var re;
+if(nu1>nu2){
+    re=nu1+nu2;
+}else if(nu1<nu2){
+    re=nu1-nu2;
+}else if(n1<=nu2){
+    re=nu1*nu2;
+}else if(nu1==nu2){
+
+    re=nu1/nu2;
+}
+console.log(re)
+}
+
+function mesa(){
+console.log("mesa 1")
+
+      }  
+     
+ function mesa1(){
+ console.log("mesa 2")      
+   }
+   
+function mesa3(){
+console.log("mesa 3")
+ }  
+ function mesa4(){
+    console.log("mesa 4")
+    
+          } 
+ function mesa5(){
+  console.log("mesa 5")
+   }  
+   function mesa6(){
+ console.log("mesa 6")
+  }  
+  function mesa7(){
+  console.log("mesa 7 ")
+  }  
+  function mesa8(){
+  console.log("mesa 8")
+  }  
+  function mesa9(){
+    console.log("mesa 9")
+    }  
+    function mesa10(){
+        console.log("mesa 10")
+        }  
+
+        ///ararys
+        var lista =new Array();
+        var uno = new Object();
+        uno.nombre="toyota";
+        uno.motor="r1256";
+        uno.color="rojo";
+        uno.modelo="2018";
+        uno.placa="srb-47y"
+        uno.chasis="52sd54";
+        uno.freno="disco";
+        var car2 = new Object();
+       car2.nombre="hihlux";
+       car2.motor="r1256";
+       car2.color="rojo";
+       car2.modelo="2018";
+       car2.placa="srb-47y"
+       car2.chasis="52sd54";
+       car2.freno="disco";
+       var car3 = new Object();
+       car3.nombre="yaris";
+       car3.motor="r1256";
+       car3.color="rojo";
+       car3.modelo="2018";
+       car3.placa="srb-47y"
+       car3.chasis="52sd54";
+       car3.freno="disco";
+       var car4 = new Object();
+       car4.nombre="combi";
+       car4.motor="r1256";
+       car4.color="rojo";
+       car4.modelo="2018";
+       car4.placa="srb-47y"
+       car4.chasis="52sd54";
+       car4.freno="disco";
+       var car5 = new Object();
+       car5.nombre="lambo";
+       car5.motor="r1256";
+       car5.color="rojo";
+       car5.modelo="2018";
+       car5.placa="srb-47y"
+       car5.chasis="52sd54";
+       car5.freno="disco";
+       var car6 = new Object();
+       car6.nombre="toyota";
+       car6.motor="r1256";
+       car6.color="rojo";
+       car6.modelo="2018";
+       car6.placa="srb-47y"
+       car6.chasis="52sd54";
+       car6.freno="disco";
+       var car7 = new Object();
+       car7.nombre="toyota";
+       car7.motor="r1256";
+       car7.color="rojo";
+       car7.modelo="2018";
+       car7.placa="srb-47y"
+       car7.chasis="52sd54";
+       car7.freno="disco";
+       var car8 = new Object();
+       car8.nombre="toyota";
+       car8.motor="r1256";
+       car8.color="rojo";
+       car8.modelo="2018";
+       car8.placa="srb-47y"
+       car8.chasis="52sd54";
+       car8.freno="disco";
+       var car9 = new Object();
+       car9.nombre="toyota";
+       car9.motor="r1259";
+       car9.color="rojo";
+       car9.modelo="2018";
+       car9.placa="srb-47y"
+       car9.chasis="52sd54";
+       car9.freno="disco";
+       var car10 = new Object();
+       car10.nombre="toyota";
+       car10.motor="r12510";
+       car10.color="rojo";
+       car10.modelo="2018";
+       car10.placa="srb-47y"
+       car10.chasis="52sd54";
+       car10.freno="disco";
+       var car11= new Object();
+       car11.nombre="toyota";
+       car11.motor="r12511";
+       car11.color="rojo";
+       car11.modelo="2018";
+       car11.placa="srb-47y"
+       car11.chasis="52sd54";
+       car11.freno="disco";
+       var car12 = new Object();
+       car12.nombre="toyota";
+       car12.motor="r12512";
+       car12.color="rojo";
+       car12.modelo="2018";
+       car12.placa="srb-47y"
+       car12.chasis="52sd54";
+       car12.freno="disco";
+       var car13 = new Object();
+       car13.nombre="toyota";
+       car13.motor="r12513";
+       car13.color="rojo";
+       car13.modelo="2018";
+       car13.placa="srb-47y"
+       car13.chasis="52sd54";
+       car13.freno="disco";
+       var car14= new Object();
+       car14.nombre="toyota";
+       car14.motor="r1256";
+       car14.color="rojo";
+       car14.modelo="2018";
+       car14.placa="srb-47y"
+       car14.chasis="52sd54";
+       car14.freno="disco";
+       var car15 = new Object();
+       car15.nombre="toyota";
+       car15.motor="r1256";
+       car15.color="rojo";
+       car15.modelo="2018";
+       car15.placa="srb-47y"
+       car15.chasis="52sd54";
+       car15.freno="disco";
+             var car16 = new Object();
+       car16.nombre="toyota";
+       car16.motor="r1256";
+       car16.color="rojo";
+       car16.modelo="2018";
+       car16.placa="srb-47y"
+       car16.chasis="52sd54";
+       car16.freno="disco";
+       var car17 = new Object();
+       car17.nombre="toyota";
+       car17.motor="r12517";
+       car17.color="rojo";
+       car17.modelo="2018";
+       car17.placa="srb-47y"
+       car17.chasis="52sd54";
+       car17.freno="disco";
+       var car18 = new Object();
+       car18.nombre="toyota";
+       car18.motor="r12518";
+       car18.color="rojo";
+       car18.modelo="2018";
+       car18.placa="srb-47y"
+       car18.chasis="52sd54";
+       car18.freno="disco";
+       var car19 = new Object();
+       car19.nombre="toyota";
+       car19.motor="r12519";
+       car19.color="rojo";
+       car19.modelo="2018";
+       car19.placa="srb-47y"
+       car19.chasis="52sd54";
+       car19.freno="disco";
+       var car20 = new Object();
+       car20.nombre="toyota";
+       car20.motor="r12520";
+       car20.color="rojo";
+       car20.modelo="2018";
+       car20.placa="srb-47y"
+       car20.chasis="52sd54";
+       car20.freno="disco";
+
+    lista.push(uno);
+    lista.push(car2);
+    lista.push(car3);
+    lista.push(car4);
+    lista.push(car5);
+    lista.push(car6);
+    lista.push(car7);
+    lista.push(car8);
+    lista.push(car9);
+    lista.push(car10);
+    lista.push(car11);
+    lista.push(car12);
+    lista.push(car13);
+    lista.push(car14);
+    lista.push(car15);
+    lista.push(car16);
+    lista.push(car17);
+    lista.push(car18);
+    lista.push(car19);
+    lista.push(car20); 
+    
